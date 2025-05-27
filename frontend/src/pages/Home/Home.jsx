1f293800affb22ce2cf82936befe99941fde3fae
psarx <mailpsarx@gmail.com>
2025-05-28 00:02:23 +0530

    first commit

---
+++ frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h2>Welcome to BMCE College</h2>
+      <p>This is the homepage of the college website.</p>
+    </div>
+  );
+};
+
+export default Home;
